2f10794baee846ab0716c35aa836b35ed15529b7
edwin <535887210@qq.com>
2026-01-10 21:26:36 +0800

    更新训练脚本：将报告目标从 tensorboard 更改为 swanlab

--- qwen/train_example.sh
+++ qwen/train_example.sh
@@ -28,5 +28,5 @@ python pretrain.py \
     --greater_is_better False \
     --bf16 \
     --gradient_checkpointing \
-    --report_to tensorboard \
+    --report_to swanlab \
     --ddp_find_unused_parameters False
